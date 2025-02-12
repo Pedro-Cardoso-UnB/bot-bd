e40b6ffec152345ca130fdcb9e2ca258580fad13
Pedro-Cardoso-UnB <117392041+Pedro-Cardoso-UnB@users.noreply.github.com>
2025-02-12 18:08:52 -0300

    adicionado comando debug, o salvador da nação

--- discord-bot.sql
+++ discord-bot.sql
@@ -1,126 +1,163 @@
 CREATE TABLE "Cargo" (
-  "id_cargo" serial PRIMARY KEY,
+  "id_cargo" integer PRIMARY KEY AUTOINCREMENT,
   "cor" varchar,
   "nome" varchar,
   "prioridade" integer
 );
 
 CREATE TABLE "UsuarioCargo" (
-  "id_usuariocargo" serial PRIMARY KEY,
+  "id_usuariocargo" integer PRIMARY KEY AUTOINCREMENT,
   "fk_id_usuario" integer NOT NULL,
-  "fk_id_cargo" integer NOT NULL
+  "fk_id_cargo" integer NOT NULL,
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario"),
+  CONSTRAINT "fk_cargo" FOREIGN KEY ("fk_id_cargo") REFERENCES "Cargo" ("id_cargo")
 );
 
 CREATE TABLE "Servidor" (
-  "id_servidor" serial PRIMARY KEY,
+  "id_servidor" integer PRIMARY KEY,
   "nome" varchar
 );
 
 CREATE TABLE "UsuarioServidor" (
-  "fk_id_usuario" integer,
-  "fk_id_servidor" integer,
-  PRIMARY KEY ("fk_id_usuario", "fk_id_servidor")
+  "fk_id_usuario" integer NOT NULL,
+  "fk_id_servidor" integer NOT NULL,
+  PRIMARY KEY ("fk_id_usuario", "fk_id_servidor"),
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario"),
+  CONSTRAINT "fk_servidor" FOREIGN KEY ("fk_id_servidor") REFERENCES "Servidor" ("id_servidor")
 );
 
 CREATE TABLE "Estatistica" (
-  "id_estatistica" serial,
   "acertos" integer,
   "total" integer,
   "streak" integer,
   "streak_max" integer,
-  "fk_id_usuario" integer,
-  PRIMARY KEY ("id_estatistica", "fk_id_usuario")
+  "fk_id_usuario" integer NOT NULL UNIQUE,
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario")
 );
 
 CREATE TABLE "ComandoLog" (
   "parametro" text,
   "comando_data" timestamp,
-  "fk_id_usuario" integer PRIMARY KEY,
-  "fk_id_comando" integer NOT NULL
+  "fk_id_usuario" integer NOT NULL,
+  "fk_id_comando" integer NOT NULL,
+  PRIMARY KEY ("fk_id_usuario", "fk_id_comando"),
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario"),
+  CONSTRAINT "fk_comando" FOREIGN KEY ("fk_id_comando") REFERENCES "Comando" ("id_comando")
 );
 
 CREATE TABLE "Comando" (
-  "id_comando" serial PRIMARY KEY,
+  "id_comando" integer PRIMARY KEY AUTOINCREMENT,
   "nome" varchar,
   "descricao" text,
   "requerimento" integer
 );
 
 CREATE TABLE "UsuarioConquista" (
-  "completo" boolean,
+  "completo" boolean DEFAULT false,
   "completo_em" timestamp,
   "fk_id_conquista" integer NOT NULL,
-  "fk_id_usuario" integer PRIMARY KEY
+  "fk_id_usuario" integer NOT NULL,
+  PRIMARY KEY ("fk_id_usuario", "fk_id_conquista"),
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario"),
+  CONSTRAINT "fk_conquista" FOREIGN KEY ("fk_id_conquista") REFERENCES "Conquista" ("id_conquista")
 );
 
 CREATE TABLE "Conquista" (
-  "id_conquista" serial PRIMARY KEY,
+  "id_conquista" integer PRIMARY KEY AUTOINCREMENT,
   "nome" varchar,
   "descricao" text,
   "recompensa" integer
 );
 
 CREATE TABLE "Modificador" (
-  "id_item" serial PRIMARY KEY,
   "fk_id_item" integer NOT NULL,
   "tipo" boolean,
   "valor" float,
   "alvo" boolean,
-  "acumulador" integer
+  "acumulador" integer DEFAULT 0,
+  CONSTRAINT "fk_modificador_item" FOREIGN KEY ("fk_id_item") REFERENCES "Item" ("id_item") ON DELETE CASCADE
 );
 
 CREATE TABLE "Item" (
-  "id_item" serial PRIMARY KEY,
+  "id_item" integer PRIMARY KEY AUTOINCREMENT,
   "nome" varchar,
-  "comprado" boolean,
+  "comprado" boolean DEFAULT false,
   "descricao" text,
   "preco" integer,
-  "aquisicao_data" timestamp
+  "aquisicao_data" timestamp DEFAULT NULL
 );
 
 CREATE TABLE "UsuarioItem" (
-  "fk_id_usuario" integer,
-  "fk_id_item" integer,
-  PRIMARY KEY ("fk_id_usuario", "fk_id_item")
+  "fk_id_usuario" integer NOT NULL,
+  "fk_id_item" integer NOT NULL,
+  PRIMARY KEY ("fk_id_usuario", "fk_id_item"),
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario"),
+  CONSTRAINT "fk_item" FOREIGN KEY ("fk_id_item") REFERENCES "Item" ("id_item")
 );
 
 CREATE TABLE "Mensagem" (
-  "id_mensagem" serial PRIMARY KEY,
+  "id_mensagem" integer PRIMARY KEY AUTOINCREMENT,
   "fk_id_usuario" integer,
   "tipo_evento" varchar,
   "texto" text,
-  "media_blob" blob
+  "media_blob" blob,
+  CONSTRAINT "fk_usuario" FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario")
 );
 
 CREATE TABLE "Usuario" (
-  "id_usuario" serial PRIMARY KEY,
+  "id_usuario" integer PRIMARY KEY,
   "nome" varchar,
   "pontos" integer,
   "saldo" integer
 );
 
--- ALTER TABLE "UsuarioServidor" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
 
--- ALTER TABLE "UsuarioServidor" ADD FOREIGN KEY ("fk_id_servidor") REFERENCES "Servidor" ("id_servidor");
+-- REGISTROS DE ITENS
+-- A: aditivo, ponto
+INSERT INTO Item (nome, descricao, preco)
+VALUES ('A', 'Item 1.', 10);
+INSERT INTO Modificador (fk_id_item, tipo, valor, alvo)
+VALUES (last_insert_rowid(), FALSE, 20, FALSE);
+-- B: aditivo, moeda
+INSERT INTO Item (nome, descricao, preco)
+VALUES ('B', 'Item 2.', 10);
+INSERT INTO Modificador (fk_id_item, tipo, valor, alvo)
+VALUES (last_insert_rowid(), FALSE, 20, TRUE);
+-- C: multiplicativo, ponto
+INSERT INTO Item (nome, descricao, preco)
+VALUES ('C', 'Item 3.', 10);
+INSERT INTO Modificador (fk_id_item, tipo, valor, alvo)
+VALUES (last_insert_rowid(), TRUE, 0.1, FALSE);
+-- D: multiplicativo, moeda
+INSERT INTO Item (nome, descricao, preco)
+VALUES ('D', 'Item 4.', 10);
+INSERT INTO Modificador (fk_id_item, tipo, valor, alvo)
+VALUES (last_insert_rowid(), TRUE, 0.2, TRUE);
+-- E: aditivo, ponto
+INSERT INTO Item (nome, descricao, preco)
+VALUES ('E', 'Item 5.', 50);
+INSERT INTO Modificador (fk_id_item, tipo, valor, alvo)
+VALUES (last_insert_rowid(), FALSE, 20, FALSE);
 
--- ALTER TABLE "Estatistica" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
+-- REGISTROS DE CARGOS
+INSERT INTO Cargo (cor, nome, prioridade)
+VALUES ('#FFD700', 'Administrador', 5);
+INSERT INTO Cargo (cor, nome, prioridade)
+VALUES ('#00BFFF', 'Moderador', 4);
+INSERT INTO Cargo (cor, nome, prioridade)
+VALUES ('#32CD32', 'Membro VIP', 3);
+INSERT INTO Cargo (cor, nome, prioridade)
+VALUES ('#FFA500', 'Membro', 2);
+INSERT INTO Cargo (cor, nome, prioridade)
+VALUES ('#808080', 'Visitante', 1);
 
--- ALTER TABLE "ComandoLog" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
+-- REGISTROS DE CONQUISTAS
+INSERT INTO Conquista (nome, descricao, recompensa)
+VALUES 
+    ('Primeiro Problema', 'Resolva o primeiro problema.', 10),
+    ('5 Corretos', 'Resolva 5 problemas corretamente.', 50),
+    ('10 Corretos', 'Resolva 10 problemas corretamente.', 100),
+    ('Sequencia de 5', 'Resolva 5 problemas corretamente em sequência.', 75),
+    ('Sequencia de 10', 'Resolva 10 problemas corretamente em sequência.', 150);
 
--- ALTER TABLE "UsuarioConquista" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
-
--- ALTER TABLE "UsuarioItem" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
-
--- ALTER TABLE "UsuarioItem" ADD FOREIGN KEY ("fk_id_item") REFERENCES "Item" ("id_item");
-
--- ALTER TABLE "Mensagem" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
-
--- ALTER TABLE "UsuarioCargo" ADD FOREIGN KEY ("fk_id_usuario") REFERENCES "Usuario" ("id_usuario");
-
--- ALTER TABLE "UsuarioCargo" ADD FOREIGN KEY ("fk_id_cargo") REFERENCES "Cargo" ("id_cargo");
-
--- ALTER TABLE "ComandoLog" ADD FOREIGN KEY ("fk_id_comando") REFERENCES "Comando" ("id_comando");
-
--- ALTER TABLE "UsuarioConquista" ADD FOREIGN KEY ("fk_id_conquista") REFERENCES "Conquista" ("id_conquista");
-
--- ALTER TABLE "Modificador" ADD FOREIGN KEY ("fk_id_item") REFERENCES "Item" ("id_item");
+-- REGISTROS DE MENSAGENS
--- main.py
+++ main.py
@@ -64,6 +64,12 @@ fetch_saldo_query = "SELECT saldo FROM Usuario WHERE id_usuario = ?"
 
 insert_user_query = "INSERT INTO Usuario(id_usuario,nome,pontos,saldo) VALUES (?,?,?,?)"
 
+delete_user_query = "DELETE FROM Usuario WHERE id_usuario = ?"
+
+pk_tabela_query = "SELECT l.name FROM pragma_table_info(?) as l WHERE l.pk = 1"
+
+n_pk_tabela_query = "SELECT l.name FROM pragma_table_info(?) as l WHERE l.pk <> 1"
+
 # End SQL Queries
 
 # TODO:
@@ -82,13 +88,15 @@ async def on_message(ctx):
     msgSize = len(ctx.content)
     sqliteConnection = sqlite3.connect('bot.db')    # DB connection
     cursor = sqliteConnection.cursor()
-    cashAmount = int(msgSize/10)
+    cashAmount = int(msgSize)
     cursor.execute(fetch_saldo_query, (author,))
     data = cursor.fetchone()
     if data == None: # User does not exist, yet. Create one.
         print("User not registered!")
         cursor.execute(insert_user_query, (author, name, 0, 0))
         cursor.execute(fetch_saldo_query, (author,))
+        sqliteConnection.commit()
+        data = cursor.fetchone()
     saldoOriginal = data[0]
     new_values = (saldoOriginal + cashAmount, author)
     cursor.execute(update_query, new_values)
@@ -118,6 +126,8 @@ async def minigame(interaction: discord.Interaction):
             print("User not registered!")
             cursor.execute(insert_user_query, (author, name, 0, 0))
             cursor.execute(fetch_saldo_query, (author,))
+            sqliteConnection.commit()
+            data = cursor.fetchone()
         saldoOriginal = data[0]
         new_values = (saldoOriginal + cashAmount, author)
         cursor.execute(update_query, new_values)
@@ -125,7 +135,7 @@ async def minigame(interaction: discord.Interaction):
         sqliteConnection.close()
         await interaction.followup.send("Creditados 10 CR")
     else:
-        await interaction.followup.send("Buxa?")
+        await interaction.followup.send("Buxa? Tá errado.")
 
 @bot.tree.command()
 async def money(interaction: discord.Interaction):
@@ -141,12 +151,76 @@ async def money(interaction: discord.Interaction):
         print("User not registered!")
         cursor.execute(insert_user_query, (author, name, 0, 0))
         cursor.execute(fetch_saldo_query, (author,))
+        sqliteConnection.commit()
+        data = cursor.fetchone()
+        
     saldoOriginal = data[0]
     sqliteConnection.commit()
     sqliteConnection.close()
     await interaction.response.send_message("Vc tem " + str(saldoOriginal) + " créditos.")
 
-    
+@bot.tree.command()
+async def reset(interaction: discord.Interaction):
+    author = interaction.user.id
+    sqliteConnection = sqlite3.connect('bot.db')    # DB connection
+    cursor = sqliteConnection.cursor()
+    cursor.execute(delete_user_query, (author,))
+    sqliteConnection.commit()
+    sqliteConnection.close()
+    await interaction.response.send_message("Te deletei da database, boa sorte começando do 0.")
+
+@bot.tree.command()
+async def debug(interaction: discord.Interaction, tarefa: int, tabela: str, pk: str): # Deleta, modifica, ou adiciona entradas no bd.
+    tarefas = ["DELETE FROM ", "UPDATE ", "INSERT INTO ", "SELECT "] # 0 = Delete, 1 = Update, 2 = Insert, 3 = Visualize
+    try:
+        tarefa_query = tarefas[tarefa]
+    except:
+        await interaction.response.send_message("Operação invalida?")
+        return
+    sqliteConnection = sqlite3.connect('bot.db')    # DB connection
+    cursor = sqliteConnection.cursor()
+    cursor.execute(pk_tabela_query, (tabela,))
+    pk_tabela = cursor.fetchone()
+    await interaction.response.send_message("Aplicando " + tarefa_query + "em " + tabela)
+    if tarefa == 0:
+        full_debug_query = tarefa_query + tabela + " WHERE " + pk_tabela[0] + " = " + pk
+        cursor.execute(full_debug_query)
+    elif tarefa == 1:
+        await interaction.followup.send("Nome da key a ser editada?")
+        key = await bot.wait_for('message', timeout=10)
+        key = key.content
+        await interaction.followup.send("Novo valor?")
+        val = await bot.wait_for('message', timeout=10)
+        val = val.content
+        full_debug_query = tarefa_query + tabela + " SET " + key + " = " + val + " WHERE " + pk_tabela[0] + " = " + pk
+        cursor.execute(full_debug_query)
+    elif tarefa == 2:
+        cursor.execute(n_pk_tabela_query, (tabela,))
+        column_names = [row[0] for row in cursor.fetchall()]
+        valores = []
+        column_names.insert(0, pk_tabela[0])
+        for i in column_names:
+            await interaction.followup.send("Valor para " + i + "?")
+            val = await bot.wait_for('message', timeout=10)
+            val = val.content
+            valores.append(val)
+        full_debug_query = tarefa_query + tabela + f"({','.join(column_names)})" + " VALUES " + f"({','.join(['?' for i in valores])})"
+        print(full_debug_query)
+        cursor.execute(full_debug_query, tuple(valores))
+    elif tarefa == 3:
+        full_debug_query = tarefa_query + "* FROM " + tabela + " WHERE " + pk_tabela[0] + " = " + pk
+        cursor.execute(full_debug_query)
+        data = cursor.fetchall()
+        await interaction.followup.send(str(data))
+        sqliteConnection.commit()
+        sqliteConnection.close()
+        return
+    sqliteConnection.commit()
+    sqliteConnection.close()
+    await interaction.followup.send("Editados os valores.")
+
+
+
 
 f = open("secret.txt", "r")
 token = f.read()
